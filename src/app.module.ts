61cb09ff9686afe22dd30fd5823899c159a98309
Santiago Sierra Cano <santiscano@gmail.com>
2024-02-08 16:15:13 -0500

    add controller app

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { WhatsappApiModule } from './whatsapp-api/whatsapp-api.module';
 import { WebhookWhatsappModule } from './webhook-whatsapp/webhook-whatsapp.module';
 import { TestModule } from './test/test.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [ WhatsappApiModule, WebhookWhatsappModule, TestModule ],
-  controllers: [],
+  controllers: [ AppController ],
   providers: [],
 })
 export class AppModule {}
